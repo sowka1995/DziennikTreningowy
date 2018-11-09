ee12921c326ecfa3bf61f6aa7f9106aeec4d9760
Bartosz Sowa <b.sowa9@gmail.com>
2018-11-09 15:41:47 +0100

    Add Home Controller

---
+++ DziennikTreningowy/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DziennikTreningowy.Controllers
+{
+    [Produces("application/json")]
+    [Route("/")]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class HomeController : Controller
+    {
+        private const string SwaggerUri = "~/swagger";
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return new RedirectResult(SwaggerUri);
+        }
+    }
+
+}
