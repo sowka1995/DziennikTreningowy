5f3026e551274fc28c0fb2f34e5805a754623b79
Bartosz Sowa <b.sowa9@gmail.com>
2018-11-10 16:40:06 +0100

    Add Generic Repository interface

---
+++ DziennikTreningowy.Core/Interfaces/IGenericRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DziennikTreningowy.Core.Interfaces
+{
+    public interface IGenericRepository<T> : IDisposable where T : class
+    {
+        T Add(T entity);
+        Task<T> AddAsync(T entity);
+        int Count();
+        Task<int> CountAsync();
+        int Delete(T entity);
+        Task<int> DeleteAsync(T entity);
+        int DeleteById(int id);
+        Task<int> DeleteByIdAsync(int id);
+        T Find(Expression<Func<T, bool>> predicate);
+        ICollection<T> FindAll(Expression<Func<T, bool>> predicate);
+        Task<ICollection<T>> FindAllAsync(Expression<Func<T, bool>> predicate);
+        Task<T> FindAsync(Expression<Func<T, bool>> predicate);
+        IQueryable<T> FindBy(Expression<Func<T, bool>> predicate);
+        Task<ICollection<T>> FindByAsync(Expression<Func<T, bool>> predicate);
+        T Get(int id);
+        Task<T> GetAsync(int id);
+        IQueryable<T> GetAll();
+        Task<ICollection<T>> GetAllAsync();  
+        void Save();
+        Task<int> SaveAsync();
+        T Update(T entity);
+        Task<T> UpdateAsync(T entity);
+    }
+}
